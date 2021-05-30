7badb11d38020ccb31147eb94992a4707fd7f4e3
Reynaldo <reynaldomoreno173@gmail.com>
2021-05-29 21:18:29 -0400

    Terminando la configuracion del proyecto

---
+++ src/containers/App.svelte
@@ -0,0 +1,16 @@
+<style>
+  :global(*) {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    background-color: #676778;
+    color: #FFFFFF;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello World</h1>
+</div>
